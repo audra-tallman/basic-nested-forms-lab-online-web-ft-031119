5d71ca75fa268bec1f6db3e87bbfefc6170aee34
Audra Tallman <audi.tallman@gmail.com>
2019-06-01 16:11:06 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -17,11 +17,11 @@ class RecipesController < ApplicationController
     redirect_to recipe
   end
 
-  def update
-    recipe = Recipe.find(params[:id])
-    recipe.update(recipe_params)
-    redirect_to recipe
-  end
+  # def update
+  #   recipe = Recipe.find(params[:id])
+  #   recipe.update(recipe_params)
+  #   redirect_to recipe
+  # end
 
   private
 
